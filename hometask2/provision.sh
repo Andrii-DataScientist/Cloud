486806cd0a74bd39e384ee6c334dfadf1ec0dea8
Andrii Varenytsia <avarenitsya@gmail.com>
2022-11-29 00:33:13 +0200

    final update lab2

--- hometask2/provision.sh
+++ hometask2/provision.sh
@@ -8,7 +8,6 @@ aws ec2 create-vpc --cidr-block 10.0.0.0/16 \
 && aws ec2 create-internet-gateway \
 && igw=$(aws ec2 describe-internet-gateways --query 'InternetGateways[0].InternetGatewayId' --output text) \
 && aws ec2 attach-internet-gateway --internet-gateway-id $igw --vpc-id $vpc_id \
-&& aws ec2 allocate-address --domain vpc \
 && alloc_id=$(aws ec2 describe-addresses --query 'Addresses[1].AllocationId' --output text) \
 && aws ec2 create-nat-gateway --subnet-id $net1_id --allocation-id $alloc_id \
 && nat=$(aws ec2 describe-nat-gateways --query 'NatGateways[-1].NatGatewayId' --output text) \
@@ -19,10 +18,10 @@ aws ec2 create-vpc --cidr-block 10.0.0.0/16 \
 && aws ec2 create-security-group --group-name security-group --description "security group for instances" --vpc-id $vpc_id \
 && sg=$(aws ec2 describe-security-groups --query 'SecurityGroups[0].GroupId' --output text) \
 && aws ec2 authorize-security-group-ingress --group-id $sg --protocol tcp --port 22 --cidr 0.0.0.0/0 \
-&& aws ec2 create-key-pair --key-name cli-PrivateKey --query 'KeyMaterial' --output text > cli-PrivateKey.pem \
-&& chmod 400 cli-PrivateKey.pem \
-&& aws ec2 run-instances --image-id ami-0533f2ba8a1995cf9 --instance-type t2.micro --count 1 --subnet-id $net1_id --security-group-ids $sg --associate-public-ip-address --key-name cli-PrivateKey \
+&& aws ec2 create-key-pair --key-name cli-keyPair0 --query 'KeyMaterial' --output text > cli-keyPair.pem \
+&& chmod 400 cli-keyPair.pem \
+&& aws ec2 run-instances --image-id ami-0533f2ba8a1995cf9 --instance-type t2.micro --count 1 --subnet-id $net1_id --security-group-ids $sg --associate-public-ip-address --key-name cli-keyPair0 \
 && instance_id=$(aws ec2 describe-instances --instance-ids --query 'Reservations[0].Instances[0].InstanceId' --output text) \
 && aws ec2 modify-vpc-attribute --enable-dns-hostnames --vpc-id $vpc_id \
-&& log=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[].Instances[].PublicDnsName' --output text) \
-&& ssh -i "cli-PrivateKey.pem" ec2-user@$log
+&& user=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[].Instances[].PublicDnsName' --output text) \
+&& echo ssh -i "cli-keyPair.pem" ec2-user@$user
