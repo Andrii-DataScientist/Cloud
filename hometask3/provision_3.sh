08fa2c202a4623a349081ec1339715b3fa7a9af2
Andrii Varenytsia <avarenitsya@gmail.com>
2022-11-29 21:06:43 +0200

    upd lab3

--- hometask3/provision_3.sh
+++ hometask3/provision_3.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-aws
+aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem \
+&& chmod 400 MyKeyPair.pem \
+&& VPC_ID=$(aws ec2 describe-vpcs --query 'Vpcs[0].VpcId' --output text) \
+&& aws ec2 create-security-group --group-name SecurityGroup --description "My security group" --vpc-id $VPC_ID \
+&& SG=$(aws ec2 describe-security-groups --query 'SecurityGroups[0].GroupId' --output text) \
+&& aws ec2 authorize-security-group-ingress --group-id $SG --protocol tcp --port 22 --cidr 0.0.0.0/0 \
+&& aws ec2 authorize-security-group-ingress --group-id $SG  --protocol tcp --port 80 --cidr 0.0.0.0/0 \
+&& aws ec2 authorize-security-group-ingress --group-id $SG  --protocol tcp --port 443 --cidr 0.0.0.0/0 \
+&& touch user_script.sh \
+&& echo '#!/bin/bash
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd' > user_script.sh \
+&& NET_ID=$(aws ec2 describe-subnets --query 'Subnets[0].SubnetId' --output text) \
+&& aws ec2 run-instances --image-id ami-0f91cedb707b09db0 --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids $SG --subnet-id $NET_ID --user-data file://user_script.sh \
+&& INST_ID=$(aws ec2 describe-instances --instance-ids --query 'Reservations[0].Instances[0].InstanceId' --output text) \
+&& aws ec2 create-tags --resources $INST_ID --tags Key=Name,Value=ApacheInstance
